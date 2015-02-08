309a64ee07456d9990ed8685259c4fc0ece5df20
John Paul Alcala <ore.wa.dragon.lord@gmail.com>
2015-02-08 21:31:08 +0800

    Added build script for making docker images.
    You need to pass in the directory name, and it will build
    each version, using the underlying directory name as the
    tag.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO_NAME=`basename $1`
+
+docker pull dlord/minecraft
+
+echo "Building $REPO_NAME"
+
+for i in $1/*/; do
+    TAG=`basename $i`
+    echo "Building dlord/$REPO_NAME:$TAG"
+    docker build -t dlord/$REPO_NAME:$TAG $i
+done
