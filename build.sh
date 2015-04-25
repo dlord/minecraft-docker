f2cd8e547a9ea4b052cf9a4964b27bfccd48c95c
John Paul Alcala <ore.wa.dragon.lord@gmail.com>
2015-04-25 16:18:54 +0800

    Forced update on base images when building.
    Signed-off-by: John Paul Alcala <ore.wa.dragon.lord@gmail.com>

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ echo "Building $REPO_NAME"
 for i in $1/*/; do
     TAG=`basename $i`
     echo "Building dlord/$REPO_NAME:$TAG"
-    docker build -t dlord/$REPO_NAME:$TAG $i
+    docker build --pull -t dlord/$REPO_NAME:$TAG $i
 done
